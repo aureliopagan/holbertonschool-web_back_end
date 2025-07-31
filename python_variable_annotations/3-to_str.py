4485098d1397a6519d4f3301d069065bb1e5d4ef
aureliopagan <aurepgst@gmail.com>
2025-07-31 16:11:57 -0400

    Updated task 4

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""to string module"""
+"""Creating string module"""
 
 
 def to_str(n: float) -> str:
     """
-    Returns the string representation of a n
+    Returns the string representation of n
     """
     return str(n)
---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates variables with specific types and values.
+"""
+
+# An integer variable with a value of 1
+a: int = 1
+
+# A float variable representing the mathematical constant pi
+pi: float = 3.14
+
+# A boolean variable indicating understanding of type annotations
+i_understand_annotations: bool = True
+
+# A string variable representing the name of the school
+school: str = "Holberton"
