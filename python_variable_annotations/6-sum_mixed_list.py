b6aaad6499b9f9a8c0174248c633decea15e597a
aureliopagan <aurepgst@gmail.com>
2025-07-31 16:16:50 -0400

    Updated task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,13 @@
 
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Adds up all the numbers in the mixed list and returns a float."""
     return sum(mxd_lst)
+
+
+if __name__ == "__main__":
+    # Test the function
+    my_list = [1, 2.5, 3, 4.5]
+    print(sum_mixed_list(my_list))  # Output: 11.0
