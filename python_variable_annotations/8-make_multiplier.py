b40aa97097e3e64d0206a063e88803ecfe428aaf
aureliopagan <aurepgst@gmail.com>
2025-07-31 16:20:48 -0400

    Updated task 8 and 9

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,8 +3,21 @@
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Takes a multiplier and returns a function that multiplies a float by it."""
+    """
+    Takes a multiplier and returns a function that multiplies
+    a float by it.
+    """
     def multiplier_func(x: float) -> float:
         return x * multiplier
     return multiplier_func
+
+
+if __name__ == "__main__":
+    # Test the function
+    times_2_5 = make_multiplier(2.5)
+    print(times_2_5(3))  # Output: 7.5
+
+    times_10 = make_multiplier(10)
+    print(times_10(5))  # Output: 50
