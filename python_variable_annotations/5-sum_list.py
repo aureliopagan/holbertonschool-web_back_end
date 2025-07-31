955919ebb9c2a8f385c721f7ae0e09155b2f591d
aureliopagan <aurepgst@gmail.com>
2025-07-31 16:14:17 -0400

    Updated task 5 and 6

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,6 +3,13 @@
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Adds up all the numbers in the list."""
     return sum(input_list)
+
+
+if __name__ == "__main__":
+    # Test the function
+    my_list = [1.5, 2.5, 3.5]
+    print(sum_list(my_list))  # Output: 7.5
