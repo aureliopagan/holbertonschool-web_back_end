a3a4ec12f0da2611a2490c403ea4d941b8e0e498
aureliopagan <aurepgst@gmail.com>
2025-07-31 16:10:03 -0400

    Updated task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-to_str = __import__('3-to_str').to_str
+"""to string module"""
 
-pi_str = to_str(3.14)
-print(pi_str == str(3.14))
-print(to_str.__annotations__)
-print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a n
+    """
+    return str(n)
