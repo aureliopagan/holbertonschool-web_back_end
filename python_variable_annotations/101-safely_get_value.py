241dcac80f4d13f3f6e6f676f3fc9570c113057e
aureliopagan <aurepgst@gmail.com>
2025-08-01 02:46:01 -0400

    Advanced

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -7,17 +7,17 @@ T = typing.TypeVar('T')
 
 
 def safely_get_value(dct: typing.Mapping,
-                    key: typing.Any,
-                    default: typing.Union[T, None] = None
-                    ) -> typing.Union[typing.Any, T]:
+                     key: typing.Any,
+                     default: typing.Union[T, None] = None
+                     ) -> typing.Union[typing.Any, T]:
     """
     Safely get a value from a dictionary-like object.
-    
+
     Args:
         dct: A mapping object (dict-like)
         key: The key to look up
         default: Default value to return if key not found
-        
+
     Returns:
         The value from the mapping if key exists, otherwise the default value
     """
