c4290e5ae44c622e7fbc09048d5a8ccebf1e8b04
aureliopagan <aurepgst@gmail.com>
2025-08-01 02:41:31 -0400

    Advanced

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module with duck-typed function for safe first element access"""
+
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing.Optional[typing.Any]:
+    """Return the first element of a sequence or None if empty"""
+    if lst:
+        return lst[0]
+    else:
+        return None
