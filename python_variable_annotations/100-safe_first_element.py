979ba197f7a58e33acf5f157417196f7dc70c45e
aureliopagan <aurepgst@gmail.com>
2025-08-01 02:42:17 -0400

    Advanced fix

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,8 @@
 import typing
 
 
-def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing.Optional[typing.Any]:
+def safe_first_element(lst: typing.Sequence[typing.Any]
+                       ) -> typing.Optional[typing.Any]:
     """Return the first element of a sequence or None if empty"""
     if lst:
         return lst[0]
