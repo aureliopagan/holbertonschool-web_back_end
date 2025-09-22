c650821c26431dc5aafc6c99fc09361996b95656
aureliopagan <aurepgst@gmail.com>
2025-09-22 17:10:57 -0400

    Implement Car and Building classes with cloning and validation features; add main files for testing

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+class Car {
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+    cloneCar() {
+      const NewCar = this.constructor[Symbol.species] || this.constructor;
+      return new NewCar(this._brand, this._motor, this._color);
+    }
+  
+    static get [Symbol.species]() {
+      return this;
+    }
+  }
+  
+  export default Car;
